a8bf101a21aeb0146b53f2f55cc137975f782a96
Zach <zaspringer@gmail.com>
2018-04-16 13:22:01 -0700

    Got generate a random number finished

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Set up correctly')
+var randomNumber;
+
+function generateNumber() {
+  randomNumber = Math.floor((Math.random() * 100) + 1);
+  var mysteryNumber = document.getElementById('mystery-number');
+  mysteryNumber.value = '?';
+}
+
+function submitGuess() {
+  console.log('submitting guess')
+}
