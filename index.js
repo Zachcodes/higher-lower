81dcabfcc14f311f92cd5e07d2aa9a0ab207e062
Zach <zaspringer@gmail.com>
2018-04-16 13:32:03 -0700

    Pushing up before lunch

--- index.js
+++ index.js
@@ -7,5 +7,23 @@ function generateNumber() {
 }
 
 function submitGuess() {
-  console.log('submitting guess')
+  var guessNumber = document.getElementById('guess-number');
+  var guessNumberValue = guessNumber.value;
+  if(!guessNumberValue) {
+    alert('You need to enter a guess first');
+  }
+  else if(guessNumberValue == randomNumber) {
+    alert('You guessed the number!')
+  }
+  else {
+    var feedbackMessage = determineHigherLower(guessNumberValue);
+    // alert(feedbackMessage);
+  }
+}
+
+function determineHigherLower(number) {
+  var difference = randomNumber - number;
+  console.log('This is the difference', difference)
+  console.log('This is the random number', randomNumber)
+  console.log('This is the number', number)
 }
